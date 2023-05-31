02d0111c98a8cae8ff96a38437d55f50cc9fd476
Unip <99147215+L-o-j-i-p@users.noreply.github.com>
2023-05-31 17:03:58 +0300

    New fileы
    Добавил файлы

---
+++ iop2/script.js
@@ -0,0 +1,215 @@
+
+function asa(a){
+    var div = a
+    div.style.display = div.style.display === "none"
+    ? "block"
+    : "none"
+}
+
+
+function saa(a){
+    var div = a
+    div.style.display = div.style.display === "none"
+    ? "flex"
+    : "none"
+}
+
+
+function asd(a){
+    var div = a
+    div.style.display = div.style.display === "none"
+    ? "inline"
+    : "none"
+}
+
+
+function a (){
+    var box1 = document.querySelector("#yutu1")
+    saa(box1)
+}
+function b (){
+    var box1 =  document.querySelector("#yutu2")
+    asd(box1)
+}
+function c(){
+    var box1 = document.querySelector("#gmail2")
+    saa(box1)
+}
+
+function d (){
+    var box1 = document.querySelector("#gmail")
+    asd(box1)
+}
+
+function e() {
+    var box1 = document.querySelector("#step1")
+    saa(box1)
+}
+
+function f() {
+    var box1 = document.querySelector("#step2")
+    asd(box1)
+}
+
+function g() {
+    var box1 = document.querySelector("#fl1")
+    saa(box1)
+}
+
+function i() {
+    var box1 = document.querySelector("#fl2")
+    asd(box1)
+}
+
+function q() {
+    var box1 = document.querySelector("#html1")
+    saa(box1)
+}
+
+
+function k() {
+    var box1 = document.querySelector("#html2")
+    asd(box1)
+}
+
+function l() {
+    var box1 = document.querySelector("#bostrap1")
+    saa(box1)
+}
+
+function m() {
+    var box1 = document.querySelector("#bostrap2")
+    asd(box1)
+}
+
+
+function n() {
+    var box1 = document.querySelector("#w31")
+    saa(box1)
+}
+
+
+function o() {
+    var box1 = document.querySelector("#w32")
+    asd(box1)
+}
+
+
+function p() {
+    var box1 = document.querySelector("#beonmax1")
+    saa(box1)
+}
+
+
+function r() {
+    var box1 = document.querySelector("#beonmax2")
+    asd(box1)
+}
+
+
+function s() {
+    var box1 = document.querySelector("#mdn1")
+    saa(box1)
+}
+
+function t() {
+    var box1 = document.querySelector("#mdn2")
+    asd(box1)
+}
+
+
+function a1() {
+    var box1 = document.querySelector("#icon81")
+    saa(box1)
+}
+
+function a2() {
+    var box1 =  document.querySelector("#icon82")
+    asd(box1)
+}
+
+function a3() {
+    var box1 = document.querySelector("#Netlify1")
+    saa(box1)
+}
+
+function a4() {
+    var box1 = document.querySelector("#Netlify2")
+    asd(box1)
+}
+
+function a5() {
+    var box1 =  document.querySelector("#colorscheme1")
+    saa(box1)   
+}
+
+function a6() {
+    var box1 = document.querySelector("#colorscheme2")
+    asd(box1)
+}
+
+function a7() {
+    var box1 = document.querySelector("#onlinefotoshop1")
+    saa(box1)
+}
+
+function a8() {
+    var box1 = document.querySelector("#onlinefotoshop2")
+    asd(box1)
+}
+
+function a9() {
+    var box1 =  document.querySelector("#codepen1")
+    saa(box1)
+}
+
+function a10() {
+    var box1 =  document.querySelector("#codepen2")
+    asd(box1)
+}
+
+function a11() {
+    var box1 = document.querySelector("#Github1")
+    saa(box1)
+}
+
+function a12() {
+    var box1 = document.querySelector("#Github2")
+    asd(box1)
+}
+
+function a13() {
+    var box1 = document.querySelector("#Googlefonts1")
+    saa(box1)
+}
+
+function a14() {
+    var box1 = document.querySelector("#Googlefonts2")
+    asd(box1)
+}
+
+function a15(){
+    var box1 = document.querySelector("#cssmatic1")
+    saa(box1)
+}
+
+function a16(){
+    var box1 = document.querySelector("#cssmatic2")
+    asd(box1)
+}
+
+
+function qq(){
+    var box1 = document.querySelector("#fast")
+    asa(box1)
+}
+
+function kk(){
+    var box1 = document.querySelector("#sitee")
+    asa(box1)
+}
+
+function fk() {
+    var box1 = document.querySelector("#info")
+    asa(box1)
+}
